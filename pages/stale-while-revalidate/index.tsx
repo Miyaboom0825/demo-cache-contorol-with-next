1db2031b0fe53a59c529370292b10782d63e91a8
miyaboom0825 <miyaboom0825@gmail.com>
2022-12-13 13:14:37 +0900

    initial commit

---
+++ pages/stale-while-revalidate/index.tsx
@@ -0,0 +1,46 @@
+import { GetServerSidePropsContext } from "next";
+
+type CacheControl = {
+  key: string;
+  value: string;
+};
+
+interface Props {
+  generatedAt: string;
+  cacheControl: CacheControl;
+}
+
+export default function StaleWhileRevalidate({ cacheControl, generatedAt }: Props) {
+  return (
+    <>
+      <ul>
+        <li>
+          {cacheControl.key} : {cacheControl.value}
+        </li>
+        <li>Generated at : {generatedAt}</li>
+      </ul>
+    </>
+  );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  if(context.query['forced-error']) {
+    throw new Error('Error occured!')
+    return
+  }
+
+  const date = new Date().toString();
+  const cacheControl = {
+    key: "Cache-Control",
+    value: "max-age=0, stale-while-revalidate=60",
+  };
+
+  context.res.setHeader(cacheControl.key, cacheControl.value);
+
+  return {
+    props: {
+      generatedAt: date,
+      cacheControl: cacheControl,
+    }, // will be passed to the page component as props
+  };
+}
